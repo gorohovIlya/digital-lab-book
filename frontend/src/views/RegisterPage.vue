e9b338381effb0fdbec8210d10a407b60ed00cca
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-08-20 16:18:40 +0800

    added v-models to form fields and created basic data function

--- frontend/src/views/RegisterPage.vue
+++ frontend/src/views/RegisterPage.vue
@@ -5,12 +5,13 @@
     </nav>
     <main>
       <div class="form-body">
-        <form>
+        <form @submit.prevent="handleSubmit">
           <!--Поле ввода фамилии-->
           <p>
             <label for="lastname">Фамилия:</label>
             <input
               type="text"
+              v-model="formData.lastname"
               name="lastname"
               id="lastname"
               required
@@ -21,6 +22,7 @@
             <label for="name">Имя:</label>
             <input
               type="text"
+              v-model="formData.name"
               name="name"
               id="name"
               required
@@ -28,18 +30,19 @@
           </p>
           <!--Поле ввода отчества-->
           <p>
-            <label for="patronimyc">Отчество:</label>
+            <label for="patronymic">Отчество:</label>
             <input
               type="text"
-              name="patronimyc"
-              id="patronimyc"
+              v-model="formData.patronymic"
+              name="patronymic"
+              id="patronymic"
               required
             >
           </p>
           <!--Поле выбора подразделения (пока хардкод)-->
           <p>
             <label for="department">Подразделение:</label>
-              <select name="department" id="department">
+              <select name="department" id="department" v-model="formData.department">
                 <option value="Подразделение_1">Подразделение 1</option>
                 <option value="Подразделение_2">Подразделение 2</option>
                 <option value="Подразделение_3">Подразделение 3</option>
@@ -48,34 +51,40 @@
           <!--Поле выбора должности (пока хардкод)-->
           <p>
             <label for="post">Должность</label>
-              <select name="post" id="post">
+              <select name="post" id="post" v-model="formData.post">
                 <option value="Должность_1">Должность 1</option>
                 <option value="Должность_2">Должность 2</option>
                 <option value="Должность_3">Должность 3</option>
               </select>
           </p>
+          <!--Поле ввода E-mail:-->
           <p>
             <label for="email">E-mail:</label>
             <input
               type="email"
+              v-model="formData.email"
               name="email"
               id="email"
               required
             >
           </p>
+          <!--Поле ввода пароля:-->
           <p>
             <label for="password">Пароль:</label>
             <input
               type="password"
+              v-model="formData.password"
               name="password"
               id="password"
               required
             >
           </p>
+          <!--Поле повторного ввода пароля-->
           <p>
             <label for="password_repeat">Повторите пароль:</label>
             <input
               type="password"
+              v-model="formData.passwordRepeat"
               name="password_repeat"
               id="password_repeat"
               required
@@ -87,3 +96,22 @@
     </main>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        lastname: '',
+        name: '',
+        patronymic: '',
+        department: '',
+        post: '',
+        email: '',
+        password: '',
+        passwordRepeat: ''
+      }
+    }
+  },
+}
+</script>

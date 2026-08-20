fedae8907eeccd3c8f28493c95360852cb656acd
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-08-20 14:14:18 +0800

    added new fields to register form

--- frontend/src/views/RegisterPage.vue
+++ frontend/src/views/RegisterPage.vue
@@ -3,10 +3,57 @@
     <nav>
       <router-link to="/">На главную</router-link>
     </nav>
-
     <main>
       <div class="form-body">
         <form>
+          <!--Поле ввода фамилии-->
+          <p>
+            <label for="lastname">Фамилия:</label>
+            <input
+              type="text"
+              name="lastname"
+              id="lastname"
+              required
+            >
+          </p>
+          <!--Поле ввода имени-->
+          <p>
+            <label for="name">Имя:</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+            >
+          </p>
+          <!--Поле ввода отчества-->
+          <p>
+            <label for="patronimyc">Отчество:</label>
+            <input
+              type="text"
+              name="patronimyc"
+              id="patronimyc"
+              required
+            >
+          </p>
+          <!--Поле выбора подразделения (пока хардкод)-->
+          <p>
+            <label for="department">Подразделение:</label>
+              <select name="department" id="department">
+                <option value="Подразделение_1">Подразделение 1</option>
+                <option value="Подразделение_2">Подразделение 2</option>
+                <option value="Подразделение_3">Подразделение 3</option>
+              </select>
+          </p>
+          <!--Поле выбора должности (пока хардкод)-->
+          <p>
+            <label for="post">Должность</label>
+              <select name="post" id="post">
+                <option value="Должность_1">Должность 1</option>
+                <option value="Должность_2">Должность 2</option>
+                <option value="Должность_3">Должность 3</option>
+              </select>
+          </p>
           <p>
             <label for="email">E-mail:</label>
             <input

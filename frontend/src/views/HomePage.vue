6d254c19853939da31427fbff1fcda1bcbb8dcbf
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-08-25 16:14:32 +0800

    added test profile link to home page and changed its style

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    <router-link to="/edit">Редактирование</router-link><br>
-    <router-link to="/authorize">Войти</router-link>
+  <div class="home-page">
+    <header class="links-container">
+      <router-link to="/edit">Редактирование</router-link>
+      <router-link to="/authorize">Войти</router-link>
+      <router-link to="/profile">Макет профиля</router-link>
+    </header>
   </div>
 </template>
+
+<style>
+  .home-page {
+    width: 100%;
+    height: 100%;
+  }
+  .links-container {
+    width: 20%;
+    height: 25%;
+    display: flex;
+    flex-direction: column;
+  }
+</style>

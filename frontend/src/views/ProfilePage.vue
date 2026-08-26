5f5fea78ced0814ccd25dcaed7fe2f37378a8ff2
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-08-26 17:11:17 +0800

    created basic of profile page main part

--- frontend/src/views/ProfilePage.vue
+++ frontend/src/views/ProfilePage.vue
@@ -6,15 +6,38 @@
       <div class="edit btn dev center-align">Редактировать</div>
       <div class="logout btn dev center-align">Выйти</div>
     </header>
+    <main class="main dev">
+      <section class="personal-info-and-image dev">
+        <div class="image-holder dev">Изображение</div>
+        <div class="personal-info dev">
+          <div class="item dev">E-mail</div>
+          <div class="item dev">Фамилия</div>
+          <div class="item dev">Имя</div>
+          <div class="item dev">Отчество</div>
+        </div>
+      </section>
+      <div class="experiments-and-departments dev">
+        <div class="departments dev">Мои подразделения</div>
+        <div class="control-panel dev">Панель управления (переключение страниц + добавить новое)</div>
+        <div class="experiments dev">Мои эксперименты</div>
+        <div class="control-panel dev">Панель управления (переключение страниц + добавить новое)</div>
+      </div>
+    </main>
   </div>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style>
+.profile-page {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  background-color: #ccc;
+}
 .dev {
-    border: 2px solid grey;
+  border: 2px solid grey;
 }
 
 .center-align {
@@ -34,6 +57,7 @@
   height: 80px;
   justify-content: space-around;
   background-color: azure;
+  margin-bottom: 10px;
 }
 .greeting {
   width: 50%;
@@ -84,6 +108,69 @@
 
 .logout:hover {
   background-color: #ba2929;
+}
+
+.main {
+  margin-left: 2%;
+  width: 96%;
+  padding: 0.5%;
+  height: 500px;
+  margin-top: 5%;
+  border-radius: 12px;
+  height: 840px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: azure;
+}
+
+.personal-info-and-image {
+  width: 33%;
+  height: 98.5%;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+}
+
+.image-holder {
+  height: 60%;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+
+.personal-info {
+  height: 38%;
+  margin-left: 5%;
+  margin-right: 5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.item {
+  height: 20%;
+}
+
+.experiments-and-departments {
+  width: 66.5%;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.departments {
+  height: 33%;
+}
+
+.experiments {
+  height: 53%;
+}
+
+.control-panel {
+  height: 6%;
+  width: 35%;
+  margin-left: 65%;
 }
 
 </style>

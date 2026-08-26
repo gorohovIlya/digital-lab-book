ee5759ef3851878d58f846e3dca899a0e1e7db31
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-08-26 16:21:08 +0800

    finished header of profile page

--- frontend/src/views/ProfilePage.vue
+++ frontend/src/views/ProfilePage.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="profile-page dev">
+  <div class="profile-page">
     <header class="header dev">
-      <div class="greeting dev"></div>
-      <div class="edit btn dev"></div>
-      <div class="logout btn dev"></div>
+      <div class="to-main btn dev center-align">На главную</div>
+      <div class="greeting dev center-align">Приветствие</div>
+      <div class="edit btn dev center-align">Редактировать</div>
+      <div class="logout btn dev center-align">Выйти</div>
     </header>
   </div>
 </template>
@@ -13,12 +14,13 @@
 
 <style>
 .dev {
-    border: 2px solid blue;
+    border: 2px solid grey;
 }
 
-.profile-page {
-  width: 100%;
-  height: 100%;
+.center-align {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .header {
@@ -26,14 +28,62 @@
   position: fixed;
   display: flex;
   flex-direction: row;
-  margin: 5% 10%;
-  padding: 3%;
-  width: 80%;
-  height: 16%;
+  margin-left: 2%;
+  width: 96%;
+  padding: 0.5%;
+  height: 80px;
+  justify-content: space-around;
+  background-color: azure;
 }
 .greeting {
-  width: 70%;
-  height: 80%;
+  width: 50%;
+  height: 60%;
+  margin-top: 0.5%;
+  font-weight: bold;
+  font-size: 22px;
+}
+.btn {
+  height: 60%;
+  margin-top: 0.5%;
+  border-radius: 10px;
+  font-weight: bold;
+  font-size: 22px;
+}
+
+.to-main {
+  background-color: #70a4d4;
+  width: 13%;
+}
+
+.edit {
+  background-color: #84d1d1;
+  width: 15%;
+}
+
+.logout {
+  background-color: darkred;
+  color: white;
+  width: 10%;
+}
+
+.btn:hover {
+  cursor: pointer;
+}
+
+.to-main:hover {
+  background-color: #aecae6;
+}
+
+.edit:hover {
+  background-color: #aee6e6;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.logout:hover {
+  background-color: #ba2929;
 }
 
 </style>
